12d12d9f008d9b19ded38e61aba0e4f6e7b1a27b
mdmake <mmake.md@gmail.com>
2022-01-08 12:37:55 +0300

    task G -- Соревнование -- бьютификация

--- sprint_4/task_G.py
+++ sprint_4/task_G.py
@@ -1,13 +1,21 @@
 import sys
 
 
-def maxLengthSubarraySumToZero(A, n):
+def maxLengthSubarrayWithEqualCount01(A, n):
 
     H = dict()
     sum = 0
     maxLen = 0
     for i in range(n):
+        # если 1 -- увеличиваем сумму, если 0 -- уменьшаем
         sum = sum + 1 if A[i] > 0 else sum - 1
+
+        # если сумма нулевая, то смотрим на длину
+        # если нет, то ищем нет ли такой же суммы ранеее -- если есть,
+        # значит пространство между этими суммами нулевое.
+        # Если ни то ни другое -- просто запоминаем эту сумму
+        # то есть -- нулевую сумму проверяем на длину, для ненулевой ищем былали такая раньше,
+        # и если нет -- запоминаем, если да -- смотрим расстояние до нее и сравниваем с максимумом
         if sum == 0:
             if maxLen < i:
                 maxLen = i+1
@@ -21,13 +29,12 @@ def maxLengthSubarraySumToZero(A, n):
 
 
 
-
 def main():
 
     n = int(sys.stdin.readline().rstrip())
     data = [int(item) for item in sys.stdin.readline().rstrip().split()]
 
-    rez = maxLengthSubarraySumToZero(data, n)
+    rez = maxLengthSubarrayWithEqualCount01(data, n)
 
     print(rez)
 
