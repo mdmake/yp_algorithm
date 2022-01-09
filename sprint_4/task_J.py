e07f28f56c3777c34bf21f9643623e6678abee0a
mdmake <mmake.md@gmail.com>
2022-01-09 18:15:48 +0300

    Task J -- Сумма четверок -- не проходит по объему памяти

--- sprint_4/task_J.py
+++ sprint_4/task_J.py
@@ -15,8 +15,32 @@ def non_effective_solution(A, x):
         history.add(x[i])
     return quadros
 
+
 def effective_solution(A, x):
-    pass
+
+    n = len(x)
+    x.sort()
+    pair = dict()
+    for i in range(n):
+        for j in range(i + 1, n):
+            summ = x[i] + x[j]
+            if summ not in pair:
+                pair[summ] = {(i, j), }
+            else:
+                pair[summ].add((i, j))
+
+    storage = set()
+    for i in range(2, n):
+        for j in range(i + 1, n):
+            target = A - x[i] - x[j]
+            if target in pair:
+                for (i_0, j_0) in pair[target]:
+                    if j_0 < i:
+                        storage.add((x[i_0], x[j_0], x[i], x[j]))
+
+    return storage
+
+
 
 
 def main():
@@ -24,7 +48,7 @@ def main():
     A = int(sys.stdin.readline().rstrip())
     data = [int(item) for item in sys.stdin.readline().rstrip().split()]
 
-    rez = non_effective_solution(A, data)
+    rez = effective_solution(A, data)
     print(len(rez))
     rez = list(rez)
     rez.sort()
