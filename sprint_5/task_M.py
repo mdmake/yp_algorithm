94290831f55b8aef4276763245452c5f1ba85490
mdmake <mmake.md@gmail.com>
2022-02-18 16:00:38 +0300

    task M

---
+++ sprint_5/task_M.py
@@ -0,0 +1,21 @@
+def sift_up(heap, idx):
+
+    if idx == 1:
+        return idx
+
+    parent_index = idx // 2
+    if heap[parent_index] < heap[idx]:
+        heap[parent_index], heap[idx] = heap[idx], heap[parent_index]
+        return sift_up(heap, parent_index)
+    else:
+        return idx
+
+
+def test():
+    sample = [-1, 12, 6, 8, 3, 15, 7]
+    assert sift_up(sample, 5) == 1
+
+
+
+
+
