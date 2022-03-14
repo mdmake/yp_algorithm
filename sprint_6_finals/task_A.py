bbd31585f33a36651e02ae0bb3d994f463537bf2
mdmake <mmake.md@gmail.com>
2022-03-14 16:46:09 +0300

    Отправленные варианты

--- sprint_6_finals/task_A.py
+++ sprint_6_finals/task_A.py
@@ -1,110 +1,118 @@
-import sys
+"""
+Номер посылки 66031278
+
+
+-- ПРИНЦИП РАБОТЫ --
+
+Я реализовал сортировку построение максимального остовного дерева при помощи алгоритма Прима.
+За счет взятия весов ребер с отрицательным знаком, ищется максимальное остовное дерево
+
+Алгоритм Прима можно описать следующим образом:
+Берется любая из вершин графа -- по умолчанию первая -- и находятся все исходящие из нее ребра.
+Из этих ребер выбирается ребро с наименьшим весом, и в остов добавляется ребро и вершина6 в которую ребро входило.
+Добавим ко множеству потенциально добавляемых рёбер все, которые исходят из новой вершины и входят в вершины,
+ещё не включённые в остов.
+Повторяем, пока в остовном дереве не будет n вершин и n-1 ребер.
+Каждый раз при добавлении ребра, подсчитываем его вес и добавляем к общей сумме
+
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+
+Мы храним все ребра в куче с поддержанием минимума.
+Если вместе с ребром в подграф добавляется новая вершина, то это ребро добавляется в остов. Если ребро соединяет две вершины,
+уже присутствующее в подмножестве остова, мы отбрасываем его из дальнейшего рассмотрения и из кучи в том числе.
+Благодаря приоритетной очереди сложность алгоритма Прима
+O(∣E∣ log∣V∣), где ∣E∣ — количество рёбер в графе, а ∣V∣ — количество вершин.
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ
+
+Нам необходимо хранить массив вершин ∣V∣ и ребер ∣E∣ остова, пространственная сложность O(∣E∣ + ∣V∣)
+"""
+
 import heapq
-
-minimum_spanning_tree = []  # Рёбра, составляющие MST.
-
-added = set()  # Множество вершин, уже добавленных в остов.
-not_added = set()  # Множество вершины, ещё не добавленных в остов.
-edges = list()  # Массив рёбер, исходящих из остовного дерева.
-all_edges = dict()
-# vertices = set()
-weight = dict()
+import sys
+from collections import defaultdict
 
 
-def add_vertex(v):
-    global edges
-    global added
-    global not_added
+class Graph:
 
-    added.add(v)
-    not_added.remove(v)
-    # Добавляем все рёбра, которые инцидентны v, но их конец ещё не в остове.
-    #
-    # Вершины not_added стоит хранить в таком контейнере,
-    # чтобы проверка (end in not_added) выполнялась эффективно.
-    #
-    # Для этого подойдёт, например, хеш-таблица.
-    # Также в некоторых языках программирования имеется контейнер set.
+    def __init__(self, vertex_count: int, init_vertex: int = 1):
+        """
+        инициализация
 
-    for (end, w) in all_edges.get(v, []):
-        if end in not_added:
-            heapq.heappush(edges, (w, (v, end)))
+        :param vertex_count: количетсво вершин в графе
+        :param init_vertex: вершина, с которой начнем поиск максимального остовного дерева
+        """
 
-    # edges += [(i, j) for (i, j) in all_edges if i == v and j in not_added]
-    # edges += graph.edges.filter(start == v, end in not_added)
+        self.added = set()  # Множество вершин, уже добавленных в остов.
+        self.not_added = set(range(1, vertex_count + 1))  # Множество вершины, ещё не добавленных в остов.
+        self.edges = list()  # Массив рёбер, исходящих из остовного дерева.
+        self.all_edges = defaultdict(list)
+        self.init_vertex = init_vertex
 
+    def add_edge_with_invert_weight(self, i: int, j: int, weight: int):
+        """
+        Добавление неориентированного ребра графа, вес ребра берется с противоположным знаком,
+        для решения нахождения максимального остовного дерева вмест минимального, не меняя основной алгоритм
 
-def extract_minimum(egs: list):
-    #me = max(egs, key=lambda item: weight[frozenset(item)])
-    w, me = heapq.heappop(egs)
-    #egs.remove(me)
-    return w, me
+        :param i: первая вершина
+        :param j: вторая вершина
+        :param weight: вес ребра
+        """
+        if i != j:
+            self.all_edges[i].append((j, -weight))
+            self.all_edges[j].append((i, -weight))
 
+    def add_vertex(self, v):
+        """
+        Добавляет все рёбра, которые инцидентны v, но их конец ещё не в остове.
 
-def find_MST():
-    summ = 0
-    #global minimum_spanning_tree
+        :param v: номер вершины
+        """
+        self.added.add(v)
+        self.not_added.remove(v)
 
-    # not_added = vertices
+        for (end, weight) in self.all_edges.get(v, []):
+            if end in self.not_added:
+                heapq.heappush(self.edges, (weight, (v, end)))
 
-    # Берём первую попавшуюся вершину.
-    v = 1
-    add_vertex(v)
+    def find_min_spanning_tree_weight(self) -> int:
+        """
+        Строит минимальное остовное дерево, подсчитывает и выдает суммарный вес его ребер
+        Если построить минимальное остовное дерево невозможно, возбуждается исключение
+        """
 
-    # пока not_added не пуст и edges не пуст:
-    while len(not_added) > 0 and len(edges) > 0:
-        # Подразумеваем, что extract_minimum извлекает минимальное ребро
-        # из массива рёбер и больше данного ребра в массива не будет.
-        w, (begin, end) = extract_minimum(edges)
+        sum_weight = 0
 
+        self.add_vertex(self.init_vertex)
 
-        if end in not_added:
-            # если e.end in not_added, то:
-            summ += w
-            #minimum_spanning_tree.append(end)
-            add_vertex(end)
+        while len(self.not_added) > 0 and len(self.edges) > 0:
+            weight, (_, end) = heapq.heappop(self.edges)
 
-    if len(not_added) > 0:
-        # если not_added не пуст, то
-        # raise Exception("Исходный граф несвязный")
-        print('Oops! I did it again')
-    # верни ошибку "Исходный граф несвязный"
-    else:
-        print(-summ)
-        #return minimum_spanning_tree
+            if end in self.not_added:
+                sum_weight += weight
+                self.add_vertex(end)
+
+        if len(self.not_added) > 0:
+            raise Exception('Oops! I did it again')
+
+        return sum_weight
 
 
 def main():
-    # количество вершин, количество ребер
+
     n, m = (int(item) for item in sys.stdin.readline().rstrip().split())
 
-    # словарь вершина - смежные вершины
+    graph = Graph(n)
 
     for _ in range(m):
-        i, j, L = map(int, sys.stdin.readline().rstrip().split())
+        graph.add_edge_with_invert_weight(*map(int, sys.stdin.readline().rstrip().split()))
 
-        # веса ребер
-        if i != j:
-
-            if i not in all_edges.keys():
-                all_edges[i] = [(j, -L), ]
-            else:
-                all_edges[i].append((j, -L))
-
-            if j not in all_edges.keys():
-                all_edges[j] = [(i, -L), ]
-            else:
-                all_edges[j].append((i, -L))
-
-
-
-    # MainDFS(s, n, graph)
-    for i in range(1, n + 1):
-        # vertices.append(i)
-        not_added.add(i)
-
-    find_MST()
-    # print(minimum_spanning_tree)
+    try:
+        max_spanning_tree_weight = graph.find_min_spanning_tree_weight()
+        print(-max_spanning_tree_weight)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
--- sprint_6_finals/task_B.py
+++ sprint_6_finals/task_B.py
@@ -1,69 +1,86 @@
+"""
+Номер посылки 66033291
+
+
+-- ПРИНЦИП РАБОТЫ --
+
+В представленной задаче, если счиать все дороги типа R -- дорогами одного направления,
+а дороги типа B -- дорогами другого направления, то можно свести задачу к задаче нахождения петель в графе
+Действительно, если из точки A в точку B можно доехать дорогами обоих типов, то в ориентированном графе
+это будет означать наличие петли с вершинами A и B.
+
+Тогда можно решить задачу методом обхода в глубину с покраской.
+
+Если при проверке смежных по исходящим дугам вершин очередная вершина окажется серой — цикл есть,
+то есть сущест вует пара городов, меду которыми есть дороги двух типов
+
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+
+В процессе поиска мы проходим по всем вершинам ∣V∣, перебирая все ребра ∣E∣, итоговая сложность O(∣E∣+ ∣V∣)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ
+
+Нам необходимо хранить массив цветов -- O(V) дополнительной памяти
+"""
+
+
 import sys
+from typing import List, Dict
+
+WHITE = -1
+GREY = 0
+BLACK = 1
 
 
-def DFS(start_vertex: int, color, vertex: dict, colorComponent):
-    stack = []
-    stack.append(start_vertex)
+def is_loop_DFS(start_vertex: int, color: List[int], graph: Dict[int, List[int]]) -> bool:
+    """
+    Поиск петель в графе
+
+    :param start_vertex: вершина начала обхода
+    :param color: массив цветов
+    :param graph: массив вершин
+    :return: результата работа алгоритма: True, если петель нет,
+             в противном случае False
+    """
+    stack = [start_vertex, ]
 
     while len(stack) > 0:
-        # Получаем из стека очередную вершину.
-        # Это может быть как новая вершина, так и уже посещённая однажды.
         v = stack.pop()
-
-
-        if color[v] == -1:
-            # print(v, end=' ')
-            # Красим вершину в серый. И сразу кладём её обратно в стек:
-            # это позволит алгоритму позднее вспомнить обратный путь по графу.
-            color[v] = 0
+        if color[v] == WHITE:
+            color[v] = GREY
             stack.append(v)
-            # Теперь добавляем в стек все непосещённые соседние вершины,
-            # вместо вызова рекурсии
-            # для каждого исходящего ребра (v,w):
-            if v in vertex:
-                for w in sorted(vertex[v], reverse=True):
-                    # возьмём вершину w
-                    if color[w] == -1:
+            if v in graph:
+                for w in graph[v]:
+                    if color[w] == WHITE:
                         stack.append(w)
-                    elif color[w] == 0:
+                    elif color[w] == GREY:
                         return False
 
-
-        elif color[v] == 0:
-            # Серую вершину мы могли получить из стека только на обратном пути.
-            # Следовательно, её следует перекрасить в чёрный.
-            color[v] = colorComponent
+        elif color[v] == GREY:
+            color[v] = BLACK
 
     return True
 
 
-def MainDFS(m, graph):
-    color = [-1] * (m + 1)
+def main_DFS(m: int, graph: Dict[int, List[int]]):
+    """
+    Поиск петель во всех компонентах связанности
 
-    # для каждого i от 0 до |V| - 1:
+    :param m: количество вершин
+    :param graph: словарь, описывающий ребра графа
+    :return:
+    """
+    color = [WHITE] * (m + 1)
 
-    component_count = 0
-    rez = True
     for v in range(1, m + 1):
-        if color[v] == -1:
-            component_count += 1
-            rez &= DFS(v, color, graph, component_count)  # Запускаем обход, стартуя с i-й вершины.
+        if color[v] == WHITE:
+            result = is_loop_DFS(v, color, graph)
+            if not result:
+                print('NO')
+                return
 
-
-    if rez:
-        print('YES')
-    else:
-        print('NO')
-
-
-    # result = [list() for _ in range(component_count)]
-    #
-    # for i, item in enumerate(color[1:]):
-    #     result[item - 1].append(i + 1)
-    #
-    # print(component_count)
-    # for i in range(component_count):
-    #     print(*result[i])
+    print("YES")
 
 
 def main():
@@ -75,7 +92,7 @@ def main():
     for i in range(1, n):
 
         for j, rb in enumerate(list(sys.stdin.readline().rstrip())):
-            k = i+j+1
+            k = i + j + 1
             if rb == 'R':
                 if i in graph:
                     graph[i].append(k)
@@ -88,10 +105,7 @@ def main():
                 else:
                     graph[k] = [i, ]
 
-    # for k in graph:
-    #     graph[k] = sorted(graph[k])
-
-    MainDFS(n, graph)
+    main_DFS(n, graph)
 
 
 if __name__ == '__main__':
