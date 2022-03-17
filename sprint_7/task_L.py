28d8bb583db868d8889e7c5e8204470395b33f27
mdmake <mmake.md@gmail.com>
2022-03-17 14:10:52 +0300

    task L Золото лепреконов-- добавил ссылку

--- sprint_7/task_L.py
+++ sprint_7/task_L.py
@@ -11,6 +11,7 @@ def main():
 
     dp = [0] * (M + 1)
 
+    # https: // www.youtube.com / watch?v = T4bY72lCQac & list = PLqM7alHXFySGMu2CSdW_6d2u1o6WFTIO -
     # https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
     for i in range(n):
         for j in range(M, weight[i]-1, -1):
