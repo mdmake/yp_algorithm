5f4f502105e33cdfab07db0c72ae6d0a460e977c
mdmake <mmake.md@gmail.com>
2022-03-17 13:11:47 +0300

    task L -- решение с пространственной памятью O(M) -- TL

--- sprint_7/task_L.py
+++ sprint_7/task_L.py
@@ -8,16 +8,19 @@ def main():
 
     weight = list(map(int, sys.stdin.readline().rstrip().split()))
 
-    dp = [[0] * (M + 1) for _ in range(n)]
+    dp0 = [0] * (M + 1)
+    dp1 = [0] * (M + 1)
 
     for i in range(n):
         for j in range(M + 1):
             if j - weight[i] >= 0:
-                dp[i][j] = max(dp[i - 1][j], weight[i] + dp[i - 1][j - weight[i]])
+                dp1[j] = max(dp0[j], weight[i] + dp0[j - weight[i]])
             else:
-                dp[i][j] = dp[i - 1][j]
+                dp1[j] = dp0[j]
 
-    print(dp[-1][-1])
+        dp0 = [item for item in dp1]
+
+    print(dp1[-1])
 
 
 if __name__ == '__main__':
